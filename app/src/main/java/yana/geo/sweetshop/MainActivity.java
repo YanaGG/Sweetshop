7f9ba4d7126db41d11fb40929e6dedc6f9c2b112
Yana Geo <94054850+YanaGG@users.noreply.github.com>
2024-03-30 22:14:23 +0200

    Initial commit

---
+++ app/src/main/java/yana/geo/sweetshop/MainActivity.java
@@ -0,0 +1,33 @@
+package yana.geo.sweetshop;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView listViewSweets;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ActionBar actionBar=getSupportActionBar();
+        if(actionBar !=null){
+            actionBar.hide();
+        }
+
+        listViewSweets = findViewById(R.id.listViewSweets);
+        listViewSweets.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+               Toast.makeText(getApplicationContext(), "Position: " + position, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
